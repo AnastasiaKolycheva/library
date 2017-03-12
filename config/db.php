880cffba3e75a24b05de6f609bb26fc07dadb402
AnastasiaKolycheva <missfury1212@gmail.com>
2017-03-12 12:14:54 +0300

    project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=missfury',
+    'username' => 'missfury',
+    'password' => '520076',
+    'charset' => 'utf8',
+];
